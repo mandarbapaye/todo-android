439bca4a689b23a83eca8507d0d20d95e5dde262
Mandar Bapaye <mandarbapaye@yahoo.com>
2014-05-20 17:17:08 -0700

    Sample changes to test AlarmManager and Notifications. (Needs further work)

--- src/com/mb/todo/ToDoMainActivity.java
+++ src/com/mb/todo/ToDoMainActivity.java
@@ -13,6 +13,8 @@ import java.util.Scanner;
 import org.apache.commons.io.FileUtils;
 
 import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
@@ -205,6 +207,25 @@ public class ToDoMainActivity extends Activity {
 			todoList.remove(itemPos + 1);
 			todoItemsAdapter.notifyDataSetChanged();
 			saveItemsToFile();
+			
+			Calendar calendar = Calendar.getInstance();
+
+			calendar.set(Calendar.MONTH, 6);
+			calendar.set(Calendar.YEAR, 2013);
+			calendar.set(Calendar.DAY_OF_MONTH, 13);
+
+			calendar.set(Calendar.HOUR_OF_DAY, 20);
+			calendar.set(Calendar.MINUTE, 48);
+			calendar.set(Calendar.SECOND, 0);
+			calendar.set(Calendar.AM_PM, Calendar.PM);
+
+			Intent myIntent = new Intent(ToDoMainActivity.this, MyReceiver.class);
+			PendingIntent pendingIntent = PendingIntent.getBroadcast(ToDoMainActivity.this, 0,	myIntent, 0);
+
+			AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+			alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+			
+			
 		}
 	}
 	
