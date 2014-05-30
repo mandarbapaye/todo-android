803207a3a94265cb6fcd7eca286d04ee16a8928d
Mandar Bapaye <mandarbapaye@yahoo.com>
2014-05-30 01:35:29 -0700

    add notification action to launch ToDo app when notification is clicked

--- src/com/mb/todo/notifications/TodoAlarmReceiver.java
+++ src/com/mb/todo/notifications/TodoAlarmReceiver.java
@@ -1,15 +1,17 @@
 package com.mb.todo.notifications;
 
 import com.mb.todo.R;
+import com.mb.todo.ToDoMainActivity;
 import com.mb.todo.model.Todo;
 import com.mb.todo.util.Utils;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
-
 import static com.mb.todo.Constants.TODO_ITEM;
 
 public class TodoAlarmReceiver extends BroadcastReceiver {
@@ -26,6 +28,10 @@ public class TodoAlarmReceiver extends BroadcastReceiver {
 		mBuilder = mBuilder.setSmallIcon(R.drawable.ic_checkmark_holo_light);
 		mBuilder = mBuilder.setContentTitle(todoItem.getTitle());
 		mBuilder = mBuilder.setContentText(todoItem.isDueDateSet() ? Utils.getDueStatus(todoItem.getDueDate()).getTitle() : context.getResources().getString(R.string.noDueDate_label));
+
+		Intent activityIntent = new Intent(context, ToDoMainActivity.class);
+		PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, activityIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+		mBuilder.setContentIntent(resultPendingIntent);
 
 		NotificationsHelper.launchReminderNotification(context, mBuilder.build(), todoItem);
 	}
